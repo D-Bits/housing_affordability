a7c5588037c94a7d0e831180c0f1f05b7477fcec
D-Bits <lockwood.da88@gmail.com>
2025-12-09 22:54:28 +0000

    Added new exploration code

---
+++ pipelines/explorations/acs1_b25064_state_exp.py
@@ -0,0 +1,28 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ### Exploratory Notebook
+# MAGIC
+# MAGIC Notebook for data exploration.
+
+# COMMAND ----------
+
+# Example: Load the Delta table using an absolute path acs1_b25064_state
+df = spark.table("usafacts_acs_housing_tenure_and_affordability_data_collection.population_and_society.homeownership_rates_by_area_1960_to_present_pivoted")
+
+display(df)
+
+# COMMAND ----------
+
+df.head()
+
+# COMMAND ----------
+
+import sys
+
+sys.path.append("/Workspace/Users/lockwood.da88@gmail.com/housing_affordability/New Pipeline 2025-12-04 22:54")
+
+# COMMAND ----------
+
+# !!! Before performing any data analysis, make sure to run the pipeline to materialize the sample datasets. The tables referenced in this notebook depend on that step.
+
+display(spark.sql("SELECT * FROM workspace.default.sample_aggregation_dec_4_2254"))
